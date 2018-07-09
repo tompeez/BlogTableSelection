1124dec54c2facf2d45c6d1232be3b7a137b8732
TimoHahn <timo.hahn@virtual7.de>
2018-07-09 16:57:21 +0200

    Navigation is working this way!

--- BTSViewController/src/de/hahn/blog/tableselect/view/TableSelectBean.java
+++ BTSViewController/src/de/hahn/blog/tableselect/view/TableSelectBean.java
@@ -1,6 +1,7 @@
 package de.hahn.blog.tableselect.view;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.el.ELContext;
 import javax.el.ExpressionFactory;
@@ -14,6 +15,7 @@ import javax.servlet.ServletContext;
 
 import oracle.adf.model.BindingContext;
 import oracle.adf.model.binding.DCIteratorBinding;
+import oracle.adf.share.logging.ADFLogger;
 import oracle.adf.view.rich.component.rich.data.RichTable;
 import oracle.adf.view.rich.component.rich.layout.RichPanelFormLayout;
 import oracle.adf.view.rich.component.rich.nav.RichButton;
@@ -32,12 +34,19 @@ import org.apache.myfaces.trinidad.event.SelectionEvent;
 import org.apache.myfaces.trinidad.model.CollectionModel;
 import org.apache.myfaces.trinidad.model.RowKeySet;
 import org.apache.myfaces.trinidad.model.RowKeySetImpl;
+import org.apache.myfaces.trinidad.util.ComponentReference;
 
 public class TableSelectBean {
-    private RichPanelFormLayout pform;
-    private RichTable regionTable;
+    private static ADFLogger log = ADFLogger.createADFLogger(TableSelectBean.class);
+    private ComponentReference<RichPanelFormLayout> pform;
+    private ComponentReference<RichTable> regionTable;
+    private ComponentReference<RichTable> countriesTable;
     private String id;
     private String name;
+    private static String[] ITERATORS = { "RegionsView1Iterator", "CountriesView1Iterator" };
+    String pageloadTrigger = "false";
+    int currentIteratorIndex = -1;
+
 
     public void setId(String id) {
         this.id = id;
@@ -58,15 +67,6 @@ public class TableSelectBean {
     public TableSelectBean() {
     }
 
-    public void onTableSelect(SelectionEvent selectionEvent) {
-        // #{bindings.RegionsView1.collectionModel.makeCurrent}
-        //add pre-processing instructions here
-        invokeMethodExpression("#{bindings.RegionsView1.collectionModel.makeCurrent}", new Object[] { selectionEvent },
-                               new Class[] { SelectionEvent.class });
-        //add post processing instructions here
-        AdfFacesContext.getCurrentInstance().addPartialTarget(getPform());
-    }
-
     public void onTableSelect2(SelectionEvent selectionEvent) {
         RichTable _table = (RichTable) selectionEvent.getSource();
         //the Collection Model is the object that provides the
@@ -80,6 +80,20 @@ public class TableSelectBean {
         //ADF table binding
         DCIteratorBinding _tableIteratorBinding = _adfTableBinding.getDCIteratorBinding();
 
+        int newIndex = getIndexFromiteratorName(_tableIteratorBinding.getName());
+        if (currentIteratorIndex != newIndex) {
+            log.info("Switching tables from " + currentIteratorIndex + " to " + newIndex);
+            // clear selection of old table nad set selection in new table
+            RowKeySet emptySet = createRowKeySet(null);
+            RichTable oldTable = getTableFromIndex(currentIteratorIndex);
+            oldTable.setSelectedRowKeys(emptySet);
+            currentIteratorIndex = newIndex;
+            AdfFacesContext.getCurrentInstance().addPartialTarget(oldTable);
+        }
+
+        if (true) {
+            _table.setSelectedRowKeys(selectionEvent.getAddedSet());
+        }
         //the role of this method is to synchronize the table component
         //selection with the selection in the ADF model
         Object _selectedRowData = _table.getSelectedRowData();
@@ -116,19 +130,25 @@ public class TableSelectBean {
     }
 
     public void setPform(RichPanelFormLayout pform) {
-        this.pform = pform;
+        this.pform = ComponentReference.newUIComponentReference(pform);
     }
 
     public RichPanelFormLayout getPform() {
-        return pform;
+        if (null != pform) {
+            return pform.getComponent();
+        }
+        return null;
     }
 
     public void setRegionTable(RichTable regionTable) {
-        this.regionTable = regionTable;
+        this.regionTable = ComponentReference.newUIComponentReference(regionTable);
     }
 
     public RichTable getRegionTable() {
-        return regionTable;
+        if (null != regionTable) {
+            return regionTable.getComponent();
+        }
+        return null;
     }
 
     public void onNavigation(ActionEvent actionEvent) {
@@ -139,23 +159,71 @@ public class TableSelectBean {
         // get the binding container
         BindingContainer bindings = BindingContext.getCurrent().getCurrentBindingsEntry();
         //f:attribute set
+        //aktuelle Row holen um first last zu erkennen
+        DCIteratorBinding iter = getIteratorFromIndex(currentIteratorIndex);
+        Row currentRow = iter.getCurrentRow();
         String adfAction = (String) navButton.getAttributes().get("adfAction");
+        String operation = adfAction + ITERATORS[currentIteratorIndex];
 
+        log.info("executing " + operation);
         //preserve default behavior #{bindings.Next.execute}
         //in code below
         //binding does not exist
-        OperationBinding opButton = (OperationBinding) bindings.get(adfAction);
+        OperationBinding opButton = (OperationBinding) bindings.get(operation);
         opButton.execute();
+        logOperationError(opButton.getErrors());
 
-        DCIteratorBinding iterBind = (DCIteratorBinding) bindings.get("RegionsView1Iterator");
+        Row nowRow = iter.getCurrentRow();
+        boolean switchIterator = false;
+        if ("Next".equals(adfAction) && (currentRow == nowRow)) {
+            //nächste tabelle verenden, alte selection löschen
+            int newIndex = (currentIteratorIndex + 1) % ITERATORS.length;
+            log.info("Switching tables from " + currentIteratorIndex + " to " + newIndex);
+            // clear selection of old table nad set selection in new table
+            RowKeySet emptySet = createRowKeySet(null);
+            RichTable oldTable = getTableFromIndex(currentIteratorIndex);
+            oldTable.setSelectedRowKeys(emptySet);
+            currentIteratorIndex = newIndex;
+            AdfFacesContext.getCurrentInstance().addPartialTarget(oldTable);
+            log.info("...setting action to first!");
+            adfAction = "First";
+            switchIterator = true;
+        } else if ("Previous".equals(adfAction) && (currentRow == nowRow)) {
+            //nächste tabelle verenden, alte selection löschen
+            int newIndex = Math.abs((currentIteratorIndex - 1) % ITERATORS.length);
+            log.info("Switching tables from " + currentIteratorIndex + " to " + newIndex);
+            // clear selection of old table nad set selection in new table
+            RowKeySet emptySet = createRowKeySet(null);
+            RichTable oldTable = getTableFromIndex(currentIteratorIndex);
+            oldTable.setSelectedRowKeys(emptySet);
+            currentIteratorIndex = newIndex;
+            AdfFacesContext.getCurrentInstance().addPartialTarget(oldTable);
+            log.info("...setting action to last!");
+            adfAction = "Last";
+            switchIterator = true;
+        }
+
+        if (switchIterator) {
+            operation = adfAction + ITERATORS[currentIteratorIndex];
+            log.info("executing " + operation);
+            //preserve default behavior #{bindings.Next.execute}
+            //in code below
+            //binding does not exist
+            opButton = (OperationBinding) bindings.get(operation);
+            opButton.execute();
+            logOperationError(opButton.getErrors());
+        }
+
+        DCIteratorBinding iterBind = (DCIteratorBinding) bindings.get(ITERATORS[currentIteratorIndex]);
         NavigatableRowIterator iterator = iterBind.getNavigatableRowIterator();
         Row nextRow = iterator.getCurrentRow();
         Key k = nextRow.getKey();
         iterBind.setCurrentRowWithKey(k.toStringFormat(true));
         RowKeySet newSet = createRowKeySet(k);
-        getRegionTable().setSelectedRowKeys(newSet);
+        RichTable _table = getTableFromIndex(currentIteratorIndex);
+        _table.setSelectedRowKeys(newSet);
         openPosition(nextRow);
-        AdfFacesContext.getCurrentInstance().addPartialTarget(getRegionTable());
+        AdfFacesContext.getCurrentInstance().addPartialTarget(_table);
     }
 
     private RowKeySet createRowKeySet(Key pKey) {
@@ -165,7 +233,7 @@ public class TableSelectBean {
         rowKeySetToCreate.add(lst);
         return rowKeySetToCreate;
     }
-    
+
     private void openPosition(Row row) {
         String id = row.getAttribute(0).toString();
         String val = row.getAttribute(1).toString();
@@ -173,4 +241,131 @@ public class TableSelectBean {
         setName(val);
         AdfFacesContext.getCurrentInstance().addPartialTarget(getPform());
     }
+
+    public void setCountriesTable(RichTable countriesTable) {
+        this.countriesTable = ComponentReference.newUIComponentReference(countriesTable);
+        ;
+    }
+
+    public RichTable getCountriesTable() {
+        if (null != countriesTable) {
+            return countriesTable.getComponent();
+        }
+        return null;
+    }
+
+    private RichTable getTableFromIndex(int index) {
+        switch (index) {
+        case 0:
+            return getRegionTable();
+        case 1:
+            return getCountriesTable();
+        }
+        return null;
+    }
+
+    private DCIteratorBinding getIteratorFromIndex(int index) {
+        if (index < 0 || index >= ITERATORS.length) {
+            return null;
+        }
+        BindingContainer bindings = BindingContext.getCurrent().getCurrentBindingsEntry();
+        DCIteratorBinding iterBind = (DCIteratorBinding) bindings.get(ITERATORS[index]);
+        return iterBind;
+    }
+
+    private RichTable getTableFromiteratorName(String name) {
+        RichTable res = null;
+        int index = 0;
+        boolean found = false;
+        do {
+            if (ITERATORS[index].equals(name)) {
+                found = true;
+            } else {
+                index++;
+            }
+        } while (index < ITERATORS.length || found);
+        if (found) {
+            res = getTableFromIndex(index);
+        }
+
+        return res;
+    }
+
+    private int getIndexFromiteratorName(String name) {
+        int res = -1;
+        int index = 0;
+        boolean found = false;
+        do {
+            if (ITERATORS[index].equals(name)) {
+                found = true;
+            } else {
+                index++;
+            }
+        } while (index < ITERATORS.length && !found);
+        if (found) {
+            res = index;
+        }
+
+        return res;
+    }
+
+    public void setPageloadTrigger(String pageloadTrigger) {
+        this.pageloadTrigger = pageloadTrigger;
+    }
+
+    public String getPageloadTrigger() {
+        if ("false".equals(pageloadTrigger)) {
+            // init first table first row as selected
+            currentIteratorIndex = 0;
+            RichTable fromIndex = getTableFromIndex(currentIteratorIndex);
+            selectFirstRowInTable(fromIndex);
+            pageloadTrigger = "true";
+        }
+        return pageloadTrigger;
+    }
+
+    public static void selectFirstRowInTable(RichTable table) {
+        // make first row current in iterator
+        CollectionModel tableModel = (CollectionModel) table.getValue();
+        JUCtrlHierBinding adfTableBinding = (JUCtrlHierBinding) tableModel.getWrappedData();
+        DCIteratorBinding tableIteratorBinding = adfTableBinding.getDCIteratorBinding();
+        tableIteratorBinding.setCurrentRowIndexInRange(0);
+
+        // explicitly select first row in table
+        RowKeySet rks = new RowKeySetImpl();
+        CollectionModel model = (CollectionModel) table.getValue();
+        model.setRowIndex(0);
+        Object key = model.getRowKey();
+        rks.add(key);
+        table.setSelectedRowKeys(rks);
+    }
+
+    private void logOperationError(List exceptions) {
+        Exception ex = null;
+        if (!exceptions.isEmpty()) {
+            ex = (Exception) exceptions.get(0);
+            log.warning(ex.getMessage(), ex);
+        }
+    }
+
+    private void makeCurrent(RichTable table, RowKeySet newCurrentRow, RowKeySet oldCurrentRow) {
+        //To make a row current, we need to create a SelectionEvent which
+        //expects the following arguments: component, unselected_keys,
+        //selected_keys. In our example, we don't have unselected keys and
+        //therefore create an empty RowSet for this
+        SelectionEvent selectionEvent = new SelectionEvent(oldCurrentRow, newCurrentRow, table);
+        selectionEvent.queue();
+        AdfFacesContext.getCurrentInstance().addPartialTarget(table);
+        //        String adfSelectionListener = "#{bindings.Departments.treeModel.makeCurrent}";
+        //        FacesContext fctx = FacesContext.getCurrentInstance();
+        //        Application application = fctx.getApplication();
+        //        ELContext elCtx = fctx.getELContext();
+        //        ExpressionFactory exprFactory = application.getExpressionFactory();
+        //
+        //        MethodExpression me = null;
+        //        me = exprFactory.createMethodExpression(elCtx, adfSelectionListener, Object.class, new Class[] { SelectionEvent.class });
+        //        me.invoke(elCtx, new Object[] { selectionEvent });
+    }
+
+
 }
